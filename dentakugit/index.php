6e71f5452afab9bf790caeff3cc61b9edd7006ce
ookawakouhei777 <ookawakouhei777@gmail.com>
2020-01-15 00:43:04 +0900

    【頭の体操】電卓アプリのプログラムを作成中、作成後このプログラムはLaravelで作成します 進捗状況→ガワだけ作成済（表示欄以外）

---
+++ dentakugit/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+  <head>
+    <link href="css/index.css" rel="stylesheet">
+  </head>
+  <body id ="index">
+     <div class="zero">
+　     <a href="#" class="button1">0</a>
+     </div>
+     <div class="one">
+　     <a href="#" class="button1">1</a>
+     </div>
+     <div class="two">
+       <a href="#" class="button1">2</a>
+     </div>
+     <div class="three">
+       <a href="#" class="button1">3</a>
+     </div>
+    <div class="four">
+       <a href="#" class="button1">4</a>
+    </div>
+    <div class="four">
+       <a href="#" class="button1">5</a>
+    </div>
+    <div class="six">
+       <a href="#" class="button1">6</a>
+    </div>
+    <div class="seven">
+       <a href="#" class="button1">7</a>
+    </div>
+    <div class="eight">
+       <a href="#" class="button1">8</a>
+    </div>
+    <div class="nine">
+       <a href="#" class="button1">9</a>
+    </div>
+     <div class="square">
+       <a href="#" class="plus">＋</a>
+    </div> 	
+    <div class="square">
+       <a href="#" class="minus">−</a>
+    </div>
+    <div class="square">
+       <a href="#" class="division">÷</a>
+    </div>
+    <div class="square">
+       <a href="#" class="Multiply">✕ </a>
+    </div>
+   <div class="equal">
+       <a href="#" class="equal">=</a>
+    </div>
+   <div class="delate">
+       <a href="#" class="delate">C</a>
+    </div>
+ </body>
+</html>
