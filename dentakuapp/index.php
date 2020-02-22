93035055ac514194595e0c24b6aa85a21f62277e
ookawakouhei777 <ookawakouhei777@gmail.com>
2020-02-22 10:27:59 +0900

    頭の体操で作った電卓で作成したアプリになります

--- dentakuapp/index.php
+++ dentakuapp/index.php
@@ -2,11 +2,13 @@
  <head>
   <meta charset="UTF-8">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+   <link href="css/index.css" rel="stylesheet">
 
 <div class="result">計算する値を入力してください</div>
 <!--<input type="number" value="10" class="input_number" placeholder="数値を入力してください">-->
 
 <!--<input type="hidden" class="input_number" value="6">-->
+<div id ="dentaku">
 <input type="button" class="input_number0" value="0">
 <input type="button" class="input_number1" value="1">
 <input type="button" class="input_number2" value="2">
@@ -18,12 +20,12 @@
 <input type="button" class="input_number8" value="8">
 <input type="button" class="input_number9" value="9">
 <input type="button" class="input_numberC" value="C">
-<input type="button" class="input_number10" value="＋">
+<input type="button" class="input_number10" value="+">
 <input type="button" class="input_number11" value="−">
 <input type="button" class="input_number12" value="×">
 <input type="button" class="input_number13" value="÷">
 <input type="button" class="input_number14" value="=">
-
+</div>
 
 
 
--- dentakuapp/test.php
+++ dentakuapp/test.php
@@ -1,11 +1,11 @@
 <?php
 /*
-  session_start();
-  $atai  = $_GET['no'];
+ $limit = 32;
+ 
+if(mb_strlen($text) > $limit) { 
+$title = mb_substr($text,0,$limit);
+echo $title. ･･･ ;
 
-  $_SESSION['atai'] .= $atai;
-
-  echo $_SESSION['atai'];
  */
 
 class dentaku
@@ -15,50 +15,45 @@ class dentaku
    public function keisan()
    {      
 
-	     session_start();
-             $_SESSION['atai'] .= $this->atai;
-             echo $_SESSION['atai'];
-	  
+	  session_start(); 
+	  $limit = 10;
+	  $_SESSION['atai'] .= $this->atai;
+	  // echo $_SESSION['atai'];
+	     if($_SESSION['atai'] =="C"){
+		     echo"delete";
+	     }elseif(mb_strlen($_SESSION['atai']) > $limit){
+		     $_SESSION['atai'] = null;
+		     echo"Error!!!9桁以上の数字は計算出来ません";
+	     }else{
+		     echo $_SESSION['atai'];
+	     }
 
-	    $search_string =array("＋","−","×","÷","C","=");
+	    $search_string =array("+","−","×","÷","C","=");
             for($i=0; $i < count($search_string); $i++){
 		    if(strpos($this->atai, $search_string[$i]) !== false){
 		    if($this->atai=="C"){
 			    $_SESSION['atai'] = null;
 			    echo $_SESSION['atai'];
 
-		    }else if($this->atai=="＋"){
-			    $_SESSION['enzan'] = "＋";
-			    $_SESSION['atai'] = str_replace('＋', '', $_SESSION['atai']);
+		    }else if($this->atai=="+"||"÷"||"×"||"−"){
+			    $_SESSION['enzan'] = $this->atai;
+			    $_SESSION['atai'] = str_replace($this->atai, '', $_SESSION['atai']);
 			    $_SESSION['atai2'] = $_SESSION['atai'];
 			    $_SESSION['atai'] = null;
-		
-		    }else if($this->atai=="÷"){
-                            $_SESSION['enzan'] = "÷";
-                            $_SESSION['atai'] = str_replace('÷', '', $_SESSION['atai']);
-                            $_SESSION['atai2'] = $_SESSION['atai'];
-			    $_SESSION['atai'] = null;
+		    }
+                 }
+		    
 
-		    }else if($this->atai=="×"){
-                            $_SESSION['enzan'] = "×";
-                            $_SESSION['atai'] = str_replace('×', '', $_SESSION['atai']);
-                            $_SESSION['atai2'] = $_SESSION['atai'];
-			    $_SESSION['atai'] = null;
 
-		    }else if($this->atai=="−"){
-                             $_SESSION['enzan'] = "−";
-                            $_SESSION['atai'] = str_replace('−', '', $_SESSION['atai']);
-                            $_SESSION['atai2'] = $_SESSION['atai'];
-                            $_SESSION['atai'] = null;
-
-		    }else if($this->atai=="="){
-			    if($_SESSION['enzan']=="＋"){
+		     if($this->atai=="="){
+			    if($_SESSION['enzan']=="+"){
 			       $syamu = $_SESSION['atai'];
-			       $syamu2 = $_SESSION['atai2'];
+			       $syamu2 = $_SESSION['atai2']; 
 			       $result = $syamu+$syamu2;
 			       $_SESSION['atai'] = null;
 			       $_SESSION['atai2'] = null;
 			       echo $result;
+			       break;
 
 		    }else if($_SESSION['enzan']=="÷"){
                                $syamu = $_SESSION['atai'];
@@ -67,6 +62,7 @@ class dentaku
                                $_SESSION['atai'] = null;
                                $_SESSION['atai2'] = null;
 			       echo $result;
+			       break;
 
 		    }else if($_SESSION['enzan']=="×"){
                                $syamu = $_SESSION['atai'];
@@ -75,25 +71,25 @@ class dentaku
                                $_SESSION['atai'] = null;
                                $_SESSION['atai2'] = null;
 			       echo $result;
+			       break;
 
-	           }else if($_SESSION['enzan']=="−"){
+		    }else if($_SESSION['enzan']=="−"){
                                $syamu = $_SESSION['atai'];
                                $syamu2 = $_SESSION['atai2'];
 			       $result = $syamu-$syamu2;
 			       $result = str_replace('-', '', $result);
                                $_SESSION['atai'] = null;
                                $_SESSION['atai2'] = null;
-                               echo $result;
+			       echo $result;
+			       break;
+	             }
+
 		  }
-
-
-		   }
 			    
-		    }
+		 }
 
-      }
    }
-}
+   }
 $dentaku = new dentaku();
 $dentaku->atai = $_GET['no'];
 $dentaku->keisan()
